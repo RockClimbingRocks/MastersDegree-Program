0ed4ae6eb55e0e01c41f15490693783a3d8f8937
RokPi <rok123.pintar2@gmail.com>
2023-03-18 14:11:35 +0100

    comit

--- Helpers/ChaosIndicators.jl
+++ Helpers/ChaosIndicators.jl
@@ -1,11 +1,13 @@
 
-# To še raymisl kakao boš naredu... Nisem zdovoljen z obliko funckcih (input/output parameteri)..
+# To še razmisl kakao boš naredu... Nisem zdovoljen z obliko funckcih (input/output parameteri)..
 module ChaosIndicators
     using SparseArrays;
     using LinearAlgebra;
     using Combinatorics
+    using ITensors
 
-    include("./FermionAlgebra.jl");
+
+    include("../Helpers/FermionAlgebra.jl");
     using .FermionAlgebra;
 
     include("../Hamiltonians/H2.jl");
@@ -15,6 +17,8 @@ module ChaosIndicators
     using .H4;
 
     
+
+    Sᵢ(x) = - abs(x)^2 * log(abs(x)^2)
 
 
     global function InformationalEntropy(L:: Int64, q′s:: Vector{Float64}, numberOfIterations:: Int64)
@@ -36,7 +40,7 @@ module ChaosIndicators
 
                 ϕ = eigvecs( Symmetric(Matrix(H₂ .+ q .* H₄)) );
                 ϕ  = ϕ[:, i₁:i₂];
-                Sₘ = sum( map(x -> - abs(x)^2 * log(abs(x)^2), ϕ) , dims=1);
+                Sₘ = sum( map(x -> Sᵢ(x), ϕ) , dims=1);
 
                 ∑Sₘ[i] += sum(Sₘ)/length(Sₘ);
             end 
@@ -74,23 +78,198 @@ module ChaosIndicators
         return r̲ ./ numberOfIterations
     end
 
+
+
+
+
+    # function ψ_as_MPS(ψ::Vector{Float64}, L::Int64, d::Int64, cutoff::Real, maxdim:: Int64)
+    #     # sites = siteinds(d,L);
+    #     # M = MPS(ψ, sites, cutoff=cutoff, maxdim=maxdim)
+    #     N = Int(log(2, length(ψ)))
+    #     X = reshape(ψ, [2 for i in 1:L]...)
+    #     return M 
+    # end
+
+
+    """
+    This function is needed to change order of dimensions because julia ic column and not row maijor language.
+    """
+    function reversedims(M)
+        N = length(size(M));
+        M′ = permutedims(M, N:-1:1);
+        return M′;
+    end
+
+
+    """
+    Naj bo ψ vektor dimenzije d^L katerega kočemo zapisati v MPS. Kjer je L število mest, in d število možnih konfiguracij spina na posameznem mestu.
+    Cutoff določa mejo, kjer zanemari ničle, maxdim pa določi maksimalno dimezijo bloka.
+    """
+    function Bipartition(ψ::Vector{Float64}, permutations:: Vector{Vector{Int64}}, L::Int64, d::Int64, cutoff::Real, maxdim:: Int64)
+
+        M′s = Vector{Any}();
+
+        for permutation in permutations
+            M′′ = reshape(ψ, [2 for i in 1:L]...);
+            M′  = permutedims(M′′, permutation);
+            M   = reversedims(reshape(M′, :, Int(d^(L÷2)) ))
+            push!(M′s, M);
+        end
+        return M′s;
+    end
+
+
+
+    """
+    Funkcija vrne entropijo prepletenosti stanja ψ. Vrednost Nₐ je število mest v bloku A, parameter je smiselen samo če je biparticija kompaktna, če je ta nekompaktna, se parameter avtomatsko nastavi na N÷2, kjer je N skupno število mest. 
+    "Permutation" od nas hoče imeti set indeksov v A bloku, ter set indeksov v bloku B.  
+    """
+    global function EntanglementEntropy(ψ::Vector{Float64}, permutations:: Vector{Vector{Int64}}, L::Int64, d::Int64=2, cutoff::Real=10^-7, maxdim::Int64= 10)
+
+        E′s = Vector{Float64}();
+
+        M′s = Bipartition(ψ, permutations, L, d, cutoff, maxdim);
+
+        for M in M′s
+            λ′s = svdvals(M);
+            filter!(x -> x > 10^-5, λ′s);
+
+            E = sum( map(x -> Sᵢ(x), λ′s) );
+            push!(E′s, E);
+        end
+        
+        return E′s;
+    end
+
+
+
+    global function EntanglementEntropy(L::Int64, q′s::Vector{Float64}, maxNumbOfIter::Int64, permutations:: Vector{Vector{Int64}}, d::Int64=2, cutoff::Real=10^-7, maxdim::Int64= 10)
+        D = binomial(L,L÷2);
+        η = 0.2;
+        i₁ = Int((D - (D*η)÷1)÷2);
+        i₂ = Int(i₁ + (D*η)÷1);
+
+        ind = FermionAlgebra.IndecesOfSubBlock(L,1/2);
+
+
+        E′s = zeros(Float64, (length(permutations), length(q′s)))
+
+        for (j,q) in enumerate(q′s)
+
+            E = [0., 0.];
+
+            for k in 1:maxNumbOfIter
+                params2 = H2.Params(L);
+                params4 = H4.Params(L);
+                
+                H₂ = H2.Ĥ(params2);
+                H₄ = H4.Ĥ(params4);
+
+                ϕ = eigvecs( Symmetric(Matrix(H₂ .+ q .* H₄)) )[:, i₁:i₂];
+
+                E0 = [0., 0.]; 
+                for ψ in eachcol(ϕ)
+                    ψ′ = zeros(Float64, Int(2^L));
+                    ψ′[ind] = ψ;
+                
+                    E0[:] .+= ChaosIndicators.EntanglementEntropy(ψ′, permutations, L) ./ length(eachcol(ϕ));
+                end
+
+                E .+= E0./maxNumbOfIter;
+            end
+
+            E′s[:,j] .= E
+        end
+        
+        return E′s;        
+    end
+
+    
+
+
+
+
 end
 
-# L = 8;
-# S = 1 /2;
-# μ = 0;
-# deviation_t = 1.;
-# deviation_U =1;
-# mean = 0.
-
-# q′s = [0.,0.1,0.3,0.5,1.,2,5,10,15]
-
-# numberOfIterations = 100
 
 
-# r = ChaosIndicators.LevelSpacingRatio(L, S, μ, deviation_t, deviation_U, mean, q′s, numberOfIterations)
+# include("../Hamiltonians/H2.jl");
+# using .H2;
 
-# println(r)
+# include("../Hamiltonians/H4.jl");
+# using .H4;
+
+# include("../Helpers/FermionAlgebra.jl");
+# using .FermionAlgebra;
+
+
+# using LinearAlgebra
+
+# L = 10;
+# q = 10;
+
+# params2 = H2.Params(L);
+# H₂ = H2.Ĥ(params2);
+
+# params4 = H4.Params(L);
+# H₄ = H4.Ĥ(params4);
+
+# H = H₂ .+ q .* H₄;
+
+# D = binomial(L,L÷2);
+# η = 0.2;
+# i₁ = Int((D - (D*η)÷1)÷2);
+# i₂ = Int(i₁ + (D*η)÷1);
+# ϕ = eigvecs(Matrix(H))[:, i₁:i₂];
+
+
+# ind = FermionAlgebra.IndecesOfSubBlock(L,1/2);
+
+# permutation1 = [1:1:L...];
+# permutation2 = [1:2:L..., 2:2:L...];
+# permutations = [permutation1, permutation2];
+
+
+# function ψ̂(ψ, L)
+#     ψ′ = zeros(Float64, Int(2^L));
+#     ψ′[ind] = ψ;
+    
+#     return ψ′
+# end
+
+# ψ′s = [ ψ̂(ψ, L) for ψ in eachcol(ϕ) ];
+
+
+# E′s = ChaosIndicators.EntanglementEntropy(ψ′s, permutations, L);
+
+
+# println( sum(E′s)/length(E′s) )
+# println(D)
+
+
+
+
+
+
+# E′s = Vector{Vector{Float64}}();
+
+
+# for ψ in eachcol(ϕ)
+#     # println(ψ)
+
+#     ψ′ = zeros(Float64, Int(2^L));
+#     ψ′[ind] = ψ;
+
+#     E = ChaosIndicators.EntanglementEntropy(ψ′, permutations, L);
+#     push!(E′s, E);
+# end
+
+
+
+# println( sum(E′s)/length(E′s) )
+# println(D)
+
+
 
 
 
